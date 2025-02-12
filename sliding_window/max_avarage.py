ef40f131df37d1acb6e110a8322be95abe350c2e
haoling-m <steven.haoling@gmail.com>
2025-02-11 16:47:31 -0800

    Add type hints and documentation for max_average function

--- sliding_window/max_avarage.py
+++ sliding_window/max_avarage.py
@@ -1,3 +1,6 @@
+from math import inf
+from typing import List
+
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         max_sum = float(-inf)
@@ -17,3 +20,14 @@ s = Solution()
 print(s.findMaxAverage([1,12,-5,-6,50,3], 4)) # 12.75
 print(s.findMaxAverage([5], 1)) # 5.0
 print(s.findMaxAverage([5, 5], 1)) # 5.0
+
+"""
+定长滑窗套路
+入-更新-出。
+
+入：下标为 i 的元素进入窗口，更新相关统计量。
+如果 i<k−1 则下个循环。
+更新：更新答案。一般是更新最大值/最小值。
+出：下标为 i−k+1 的元素离开窗口，更新相关统计量
+
+"""
