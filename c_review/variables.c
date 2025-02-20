c56783946e51410873706ad4b64ce33b486147d6
haoling-m <steven.haoling@gmail.com>
2025-02-20 11:28:08 -0800

    start c review

---
+++ c_review/variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    float b = 3.14;
+    char c = 'A';
+
+    printf("a = %d, b = %.2f, c = %c\n", a, b, c);
+    return 0;
+}
+
+// Output
+// a = 10, b = 3.14, c = A
