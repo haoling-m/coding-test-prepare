98b2fb03edce0e35f7d425ecf9738470b2f95519
haoling-m <steven.haoling@gmail.com>
2025-02-22 08:26:11 -0800

    Add sum function and update pass_ref.c documentation for call by reference

--- c_review/pass_ref.c
+++ c_review/pass_ref.c
@@ -1,5 +1,13 @@
-// C program to show use of 
+// C program to show use of C++ has reference type
+// C does not support reference type but it can be
 // call by Reference
+// C just like C++ supports call by reference
+// using pointers. In call by reference, the  
+// address of the variables is passed to the
+// function. So, the changes made in the
+// function reflect in the original variables.
+// In the below code, we have a function swap
+// which takes two pointers as arguments and
 #include <stdio.h>
 
 void swap(int *var1, int *var2)
@@ -20,3 +28,27 @@ int main()
           var1, var2);
   return 0;
 }
+
+/*
+void swapNums(int &x, int &y) {
+  int z = x;
+  x = y;
+  y = z;
+}
+
+int main() {
+  int firstNum = 10;
+  int secondNum = 20;
+
+  cout << "Before swap: " << "\n";
+  cout << firstNum << secondNum << "\n";
+
+  // Call the function, which will change the values of firstNum and secondNum
+  swapNums(firstNum, secondNum);
+
+  cout << "After swap: " << "\n";
+  cout << firstNum << secondNum << "\n";
+
+  return 0;
+}
+*/
